99370371bd2bcfa3f2ae1602b846abf9ca7b1fb7
AkitoAndo <ee207085@meiji.ac.jp>
2023-03-11 16:04:31 +0900

    ejsでhello world

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 // app.js
+"use strict";
 
 const express = require('express')
+
 const app = express()
+app.set("view engine", "ejs");
+app.set("views", "./src/views");
+app.use(express.static("./src/public"));
+
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render("index");
 })
 
 app.listen(port, () => {
